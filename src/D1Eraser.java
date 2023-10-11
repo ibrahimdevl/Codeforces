95573338270299eb999af84383131eee9092577e
ibrahimdevl <ibrahimhoussem2000@gmail.com>
2023-10-11 23:30:29 +0100

    1873A - Short Sort

--- src/D1Eraser.java
+++ src/D1Eraser.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class D1Eraser {
+import java.io.IOException;
+import java.util.Scanner;
+
+// link: https://codeforces.com/problemset/problem/1873/D
+public class D1Eraser {
+    public static void main(String args[]) throws IOException {
+        int n, t, k;
+        Scanner myInput = new Scanner( System.in );
+        t = myInput.nextInt();
+
+        char s [];
+        while (t-- >= 1) {
+            n = myInput.nextInt();
+            k = myInput.nextInt();
+            s = myInput.next().toCharArray();
+            int minTimes = 0;
+            for(int i=0; i<n;) {
+                if(s[i] == 'B') {
+                    minTimes++;
+                    i += k;
+                } else i++;
+            }
+
+            System.out.print(minTimes + "\n");
+
+        }
+    }
 }
--- src/ShortSort.java
+++ src/ShortSort.java
@@ -1,2 +1,21 @@
+import java.util.Scanner;
+
 public class ShortSort {
+    public static void main(String args[]){
+        int t;
+        Scanner myInput = new Scanner( System.in );
+        t = myInput.nextInt();
+        char s[];
+        while (t-- > 0) {
+            s = myInput.next().toCharArray();
+            if(s[0]=='a' || s[1]=='b' || s[2]=='c'){
+                System.out.println("YES");
+            }
+            else {
+                System.out.println("NO");
+            }
+        }
+    }
+
+
 }
